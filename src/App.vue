3ee28d4205c9a21f729bd5499efafa6cb5c26072
smcmo <starskillz13@gmail.com>
2023-03-10 12:58:44 -0600

    Couple fixes
    - Fix light mode text readibility
    - Move messages to JSON file rather than being hard coded

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,3 @@
-<script>
-import DiscordMessage from './components/DiscordMessage.vue';
-export default {
-	components: {
-		DiscordMessage,
-	},
-};
-</script>
-
 <template>
 	<div class="opening-text">
 		<h1>Hey, this is <span class="green title">#modrinth-discussion</span>!</h1>
@@ -15,24 +6,24 @@ export default {
 			non-Modrinth stuff in the #modrinth-discussion channel! 😲
 		</p>
 		<p>
-			Do you have any idea what you've done!? The Modrinth team works hard to keep
-			that channel on-topic! Who do you think you are, just waltzing on in and
-			talking about something that's not Modrinth?? Just look how hard they have
-			to work to keep this channel a civilized place:
+			Do you have any idea what you've done!? The Modrinth team works hard to
+			keep that channel on-topic! Who do you think you are, just waltzing on in
+			and talking about something that's not Modrinth?? Just look how hard they
+			have to work to keep this channel a civilized place:
 		</p>
 	</div>
 	<div class="featured-messages">
-		<DiscordMessage author="Emma (triphora)" username="triphora">
-			This is not modrinth discussion
+		<DiscordMessage
+			v-for="message in featuredMessages"
+			:key="message"
+			:author="message.author"
+			:username="message.username"
+			:avatar="message.avatar"
+			:color="message.color"
+		>
+			{{ message.message }}
 		</DiscordMessage>
-		<DiscordMessage author="Emma (triphora)" username="triphora">
-			emma sees off topic in modrinth-discussion, emma says go to #lounge
-		</DiscordMessage>
-		<DiscordMessage author="Emma (triphora)" username="triphora">
-			Beep beep beep ... Oh, would you hear that, it's the "This is
-			#modrinth-discussion " alarm
-		</DiscordMessage>
-		</div>
+	</div>
 	<p>I mean...ok. So it's really not all that serious mostly.</p>
 	<p>
 		And the purpose of this website is not to scold you for accidentally
@@ -45,205 +36,49 @@ export default {
 		list:
 	</p>
 	<div class="all-messages">
-		<DiscordMessage author="Emma (triphora)" username="triphora">
-			#modrinth-discussion is hydrophobic, #lounge is far more hydrophilic for this type of discussion
+		<DiscordMessage
+			v-for="message in allMessages"
+			:key="message"
+			:author="message.author"
+			:username="message.username"
+			:avatar="message.avatar"
+			:color="message.color"
+		>
+			{{ message.message }}
 		</DiscordMessage>
-		<DiscordMessage author="Emma (triphora)" username="triphora">
-			I don't want to be strict here but the amount of off-topic conversation in a
-			channel named #modrinth-discussion is a bit baffling
-		</DiscordMessage>
-		<DiscordMessage author="Emma (triphora)" username="triphora">
-			Remember y'all we're in #modrinth-discussion
-		</DiscordMessage>
-		<DiscordMessage author="Emma (triphora)" username="triphora">
-			Hate to interrupt but do keep in mind this is #modrinth-discussion not
-			#lounge :tiny_pumpkin:
-		</DiscordMessage>
-		<DiscordMessage author="Emma (triphora)" username="triphora">
-			lettuce move to #lounge
-		</DiscordMessage>
-		<DiscordMessage author="Emma (triphora)" username="triphora">
-			--&gt lounge &lt--
-		</DiscordMessage>
-		<DiscordMessage author="Emma (triphora)" username="triphora">
-			we are in the modrinth channel, go to #lounge
-		</DiscordMessage>
-		<DiscordMessage author="Emma (triphora)" username="triphora">
-			#lounge please
-		</DiscordMessage>
-		<DiscordMessage author="Emma (triphora)" username="triphora">
-			---&gt #lounge for the mods discussion
-		</DiscordMessage>
-		<DiscordMessage author="Emma (triphora)" username="triphora">
-			Can we like<br />
-			stop talking about this now<br />
-			This isn't modrinth discussion, but I don't want this conversation in
-			#lounge either
-		</DiscordMessage>
-		<DiscordMessage author="Emma (triphora)" username="triphora">
-			#lounge pls
-		</DiscordMessage>
-		<DiscordMessage author="Emma (triphora)" username="triphora">
-			we're definitely in #lounge territory now
-		</DiscordMessage>
-		<DiscordMessage author="Emma (triphora)" username="triphora">
-			we should be in #lounge btw
-		</DiscordMessage>
-		<DiscordMessage author="Emma (triphora)" username="triphora">
-			Go, #lounge
-		</DiscordMessage>
-		<DiscordMessage author="Emma (triphora)" username="triphora">
-			#lounge y'all 😛
-		</DiscordMessage>
-		<DiscordMessage author="Emma (triphora)" username="triphora">
-			you two, #lounge
-		</DiscordMessage>
-		<DiscordMessage author="Emma (triphora)" username="triphora">
-			boop: #lounge
-		</DiscordMessage>
-		<DiscordMessage author="Emma (triphora)" username="triphora">
-			let's head over to #lounge
-		</DiscordMessage>
-		<DiscordMessage author="Emma (triphora)" username="triphora">
-			This has been off-topic for a long time, please use #lounge
-		</DiscordMessage>
-		<DiscordMessage author="Emma (triphora)" username="triphora">
-			okay, #lounge now
-		</DiscordMessage>
-		<DiscordMessage author="Emma (triphora)" username="triphora">
-			go to #lounge , now please
-		</DiscordMessage>
-		<DiscordMessage author="Emma (triphora)" username="triphora">
-			anyway, this is starting to be (and has been for a while) #lounge territory
-		</DiscordMessage>
-		<DiscordMessage author="Emma (triphora)" username="triphora">
-			Y'all can continue this in #lounge
-		</DiscordMessage>
-		<DiscordMessage author="Emma (triphora)" username="triphora">
-			This is #lounge stuff now
-		</DiscordMessage>
-		<DiscordMessage author="Emma (triphora)" username="triphora">
-			this still isn't #lounge
-		</DiscordMessage>
-		<DiscordMessage author="Emma (triphora)" username="triphora">
-			Anyway, we should go to #lounge if we're to continue
-		</DiscordMessage>
-		<DiscordMessage author="Emma (triphora)" username="triphora">
-			μου αρέσει να λέω ότι είμαι σε επίπεδο Α2 ή Β1 ελληνομαθειών, αλλά πρέπει να
-			κάνουμε αυτό το διάλογο στο #lounge
-		</DiscordMessage>
-		<DiscordMessage author="Emma (triphora)" username="triphora">
-			This is off-topic for this channel, feel free to head to #lounge
-		</DiscordMessage>
-		<DiscordMessage author="Emma (triphora)" username="triphora">
-			let's go to #lounge
-		</DiscordMessage>
-		<DiscordMessage author="Emma (triphora)" username="triphora">
-			I'll answer in #lounge
-		</DiscordMessage>
-		<DiscordMessage author="Emma (triphora)" username="triphora">
-			also let's head to #lounge
-		</DiscordMessage>
-		<DiscordMessage author="Emma (triphora)" username="triphora">
-			But still, better in #lounge than here :&#41
-		</DiscordMessage>
-		<DiscordMessage author="Emma (triphora)" username="triphora">
-			#lounge is indeed the best place for this
-		</DiscordMessage>
-		<DiscordMessage author="Emma (triphora)" username="triphora">
-			#lounge and please be civil with such a discussion
-		</DiscordMessage>
-		<DiscordMessage author="Emma (triphora)" username="triphora">
-			#lounge :P
-		</DiscordMessage>
-		<DiscordMessage author="Emma (triphora)" username="triphora">
-			#lounge :irritatered:
-		</DiscordMessage>
-		<DiscordMessage author="Emma (triphora)" username="triphora">
-			Btw, I do like your ideas, but you may want to go to #lounge as this is out of the scope of Modrinth; this is the channel for Modrinth discussion :&#41
-		</DiscordMessage>
-		<DiscordMessage author="Emma (triphora)" username="triphora">
-			Y'all might want to go to #lounge btw :tiny_pumpkin:
-		</DiscordMessage>
-		<DiscordMessage author="Emma (triphora)" username="triphora">
-			this is getting #lounge
-		</DiscordMessage>
-		<p>
-			#SaveTriphoraFromHavingToType#Lounge20TimesADay
-		</p>
-		<DiscordMessage author="Geometrically" username="Geometrically">
-			pls  move to #lounge
-		</DiscordMessage>
-		<DiscordMessage author="Geometrically" username="Geometrically">
-			this is getting pretty off topic for Modrinth, if you guys could move to #lounge that would be great
-		</DiscordMessage>
-		<DiscordMessage author="Geometrically" username="Geometrically">
-			#lounge lol
-		</DiscordMessage>
-		<DiscordMessage author="Prospector" username="Prospector">
-			I like how #modrinth-discussion is never modrinth discussion but #lounge is
-		</DiscordMessage>
-		<DiscordMessage author="Prospector" username="Prospector">
-			maybe let's move this to a thread in #lounge
-		</DiscordMessage>
-		<DiscordMessage author="Prospector" username="Prospector">
-			#lounge #lounge #lounge ‼️‼️‼️
-		</DiscordMessage>
-		<DiscordMessage author="Prospector" username="Prospector">
-			guys <i>please</i> #lounge
-		</DiscordMessage>
-		<DiscordMessage author="Prospector" username="Prospector">
-			➡️ ➡️ ➡️ lounge ⬅️ ⬅️ ⬅️
-		</DiscordMessage>
-		<DiscordMessage author="Prospector" username="Prospector">
-			we should probably go to #lounge
-		</DiscordMessage>
-		<DiscordMessage author="Prospector" username="Prospector">
-			yeah, #lounge
-		</DiscordMessage>
-		<DiscordMessage author="languages guy (kinda)" username="Brawaru" color="#a95bf8">
-			it's all fun of course but this is #modrinth-discussion pls folks 🥹
-		</DiscordMessage>
-		<DiscordMessage author="languages guy (kinda)" username="Brawaru" color="#a95bf8">
-			cool, but this is #modrinth-discussion. if you want to talk about developing stuff like that, we have #technology :&#41
-		</DiscordMessage>
-		<DiscordMessage author="languages guy (kinda)" username="Brawaru" color="#a95bf8">
-			we should switch to the actual lounge, #modrinth-discussion
-		</DiscordMessage>
-		<DiscordMessage author="languages guy (kinda)" username="Brawaru" color="#a95bf8">
-			could've just used lounge-2 (#modrinth-discussion) for that tbh
-		</DiscordMessage>
-		<DiscordMessage author="languages guy (kinda)" username="Brawaru" color="#a95bf8">
-			my fellow amoguses, this is #modrinth-discussion
-		</DiscordMessage>
-		<DiscordMessage author="languages guy (kinda)" username="Brawaru" color="#a95bf8">
-			➡️ #lounge
-		</DiscordMessage>
-		<DiscordMessage author="languages guy (kinda)" username="Brawaru" color="#a95bf8">
-			👉 #lounge
-		</DiscordMessage>
-		<DiscordMessage author="languages guy (kinda)" username="Brawaru" color="#a95bf8">
-			please move to #lounge with that discussion
-		</DiscordMessage>
-		<DiscordMessage author="languages guy (kinda)" username="Brawaru" color="#a95bf8">
-			anuway pls #lounge
-		</DiscordMessage>
-		<DiscordMessage author="languages guy (kinda)" username="Brawaru" color="#a95bf8">
-			this discussion should go to #lounge<br>
-			ye
-		</DiscordMessage>
-		<DiscordMessage author="languages guy (kinda)" username="Brawaru" color="#a95bf8">
-			that's unrelated to modrinth, discuss that in #lounge
-		</DiscordMessage>
-		<DiscordMessage author="languages guy (kinda)" username="Brawaru" color="#a95bf8">
-			it's #lounge now
-		</DiscordMessage>
+		<p>#SaveTriphoraFromHavingToType#Lounge20TimesADay</p>
 	</div>
-	<p>Hey, you scrolled all the way down! You should <a href="https://discord.gg/fm88jhzEbt">join my Discord</a> and see what other cool stuff I make!</p>
+	<p>
+		Hey, you scrolled all the way down! You should
+		<a href="https://discord.gg/fm88jhzEbt">join my Discord</a> and see what
+		other cool stuff I make!
+	</p>
 	<footer>
-		Made in 2 hours with Vue and probably not enough sleep. By <a href="https://github.com/smcmo">beansquared (smcmo)</a>. This site is not associated or endorsed by Modrinth, Rinth Inc or any of it's staff.
+		Made in 2 hours with Vue and probably not enough sleep. By
+		<a href="https://github.com/smcmo">beansquared (smcmo)</a>. This site is not
+		associated or endorsed by Modrinth, Rinth Inc or any of it's staff.
 	</footer>
 </template>
+
+<script>
+import DiscordMessage from './components/DiscordMessage.vue';
+import messages from '../messages.json';
+export default {
+	components: {
+		DiscordMessage,
+	},
+	data() {
+		return {
+			featuredMessages: [],
+			allMessages: [],
+		};
+	},
+	created() {
+		this.featuredMessages = messages.featured;
+		this.allMessages = messages.all;
+	},
+};
+</script>
 
 <style scoped>
 h1,
@@ -261,12 +96,13 @@ p {
 	margin-top: 5rem;
 }
 
-.featured-messages, .all-messages {
+.featured-messages,
+.all-messages {
 	margin: 5rem 0;
 }
 
 footer {
-	background-color: var(--vt-c-black-mute);
+	background-color: var(--color-background-mute);
 	padding: 1rem;
 	width: 100%;
 	text-align: center;
--- src/components/DiscordMessage.vue
+++ src/components/DiscordMessage.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="discord-message">
 		<div class="inner-content">
-			<img :src="avatarUrl" alt="" id="author-icon" />
+			<img :src="avatar" alt="" id="author-icon" />
 			<div class="text-content">
 				<p class="author" :style="`color: ${color}`">{{ author }}</p>
 				<p class="message">
@@ -14,46 +14,14 @@
 
 <script>
 export default {
-	props: ['author', 'username', 'color'],
-	data() {
-		return {
-			avatarUrl: '',
-			avatarCache: [],
-		};
-	},
-	async created() {
-		const cachedData = this.avatarCache.find(
-			(thingy) => (thingy.username = this.username)
-		);
-		if (cachedData) {
-			this.avatarUrl = cachedData.avatarUrl;
-			return;
-		}
-
-		console.log('making api call');
-		const data = await fetch(
-			`https://api.modrinth.com/v2/user/${this.username}`,
-			{
-				headers: {
-					'user-agent':
-						'smcmo/this-is-modrinth-discussion (this-is-modrinth-disscussion.com)',
-				},
-			}
-		);
-		const jsonData = await data.json();
-		this.avatarCache.push({
-			username: this.username,
-			avatarUrl: jsonData.avatar_url,
-		});
-		this.avatarUrl = jsonData.avatar_url;
-	},
+	props: ['author', 'username', 'color', 'avatar'],
 };
 </script>
 
 <style scoped>
 .discord-message {
 	border-radius: 1rem;
-	background-color: var(--vt-c-black-soft);
+	background-color: var(--color-background-mute);
 	margin: 0.5rem 1rem;
 }
 
@@ -65,10 +33,6 @@ export default {
 
 .text-content {
 	margin-left: 1rem;
-}
-
-.author {
-	color: #1bd96a;
 }
 
 #author-icon {
