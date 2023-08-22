7527d927732bf6956b98c218dec393cf43b1caa1
smcmo <shanemcmonigal@gmail.com>
2023-08-21 22:43:24 -0500

    New message and banner message

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
 	<div class="opening-text">
 		<h1>Hey, this is <span class="green title">#modrinth-discussion</span>!</h1>
+		<div class="message-box">
+			<DiscordMessage author="Emma (triphora)" username="triphora" avatar="https://avatars.githubusercontent.com/u/60205699?v=4" color="#1bd96a">
+				Oh, you're new here, so you haven't even seen https://thisismodrinthdiscussion.com/
+			</DiscordMessage>
+		</div>
+		<p>If you're reading this, you've likely been caught red-handed talking about non-Modrinth stuff in the #modrinth-discussion channel! 😲</p>
 		<p>
-			If you're reading this, you've likely been caught red-handed talking about
-			non-Modrinth stuff in the #modrinth-discussion channel! 😲
-		</p>
-		<p>
-			Do you have any idea what you've done!? The Modrinth team works hard to
-			keep that channel on-topic! Who do you think you are, just waltzing on in
-			and talking about something that's not Modrinth?? Just look how hard they
-			have to work to keep this channel a civilized place:
+			Do you have any idea what you've done!? The Modrinth team works hard to keep that channel on-topic! Who do you think you are, just waltzing on in and
+			talking about something that's not Modrinth?? Just look how hard they have to work to keep this channel a civilized place:
 		</p>
 	</div>
 	<div class="message-box">
@@ -25,26 +25,16 @@
 		</DiscordMessage>
 	</div>
 	<p>I mean...ok. So it's really not all that serious mostly.</p>
-	<p>
-		And the purpose of this website is not to scold you for accidentally
-		drifting into other topics, as Emma aptly explains:
-	</p>
+	<p>And the purpose of this website is not to scold you for accidentally drifting into other topics, as Emma aptly explains:</p>
 	<div class="message-box">
-		<DiscordMessage
-			author="Emma (triphora)"
-			username="triphora"
-			avatar="https://avatars.githubusercontent.com/u/60205699?v=4"
-			color="#1bd96a"
-		>
+		<DiscordMessage author="Emma (triphora)" username="triphora" avatar="https://avatars.githubusercontent.com/u/60205699?v=4" color="#1bd96a">
 			y'all he's not saying we're banning people who go offtopic<br />
 			if that were the case we would be banning the entire server
 		</DiscordMessage>
 	</div>
 	<p>
-		This is actually mostly just a repository of my personal favorite messages
-		from the Modrinth staff (mostly Emma/triphora) telling people they've gone
-		off-track. Hey, maybe you'll enjoy them as much as I do. Enjoy a nice fat
-		list:
+		This is actually mostly just a repository of my personal favorite messages from the Modrinth staff (mostly Emma/triphora) telling people they've gone
+		off-track. Hey, maybe you'll enjoy them as much as I do. Enjoy a nice fat list:
 	</p>
 	<div class="message-box">
 		<DiscordMessage
@@ -61,19 +51,17 @@
 	</div>
 	<p>
 		Hey, you scrolled all the way down! You should
-		<a href="https://discord.gg/fm88jhzEbt">join my Discord</a> and see what
-		other cool stuff I make!
+		<a href="https://discord.gg/fm88jhzEbt">join my Discord</a> and see what other cool stuff I make!
 	</p>
 	<footer>
 		Made in 2 hours with Vue and probably not enough sleep. By
-		<a href="https://github.com/smcmo">beansquared (smcmo)</a>. This site is not
-		associated or endorsed by Modrinth, Rinth Inc or any of it's staff.
+		<a href="https://github.com/smcmo">beansquared (smcmo)</a>. This site is not associated or endorsed by Modrinth, Rinth Inc or any of it's staff.
 	</footer>
 </template>
 
 <script>
-import DiscordMessage from './components/DiscordMessage.vue';
-import messages from '../messages.json';
+import DiscordMessage from './components/DiscordMessage.vue'
+import messages from '../messages.json'
 export default {
 	components: {
 		DiscordMessage,
@@ -82,13 +70,13 @@ export default {
 		return {
 			featuredMessages: [],
 			allMessages: [],
-		};
+		}
 	},
 	created() {
-		this.featuredMessages = messages.featured;
-		this.allMessages = messages.all;
+		this.featuredMessages = messages.featured
+		this.allMessages = messages.all
 	},
-};
+}
 </script>
 
 <style scoped>
